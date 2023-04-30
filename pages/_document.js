24f68d28bb06be5cbf8fa7355a44c8a0afa4c2f3
NofarGarashi <102257064+NofarGarashi@users.noreply.github.com>
2023-04-30 10:38:14 +0300

    change width

--- pages/_document.js
+++ pages/_document.js
@@ -11,11 +11,13 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+     
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap"
           rel="stylesheet"
         />
+         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
       </Head>
       <body>
         <Main />
